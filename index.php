efbaad0b927b27b7b69dc23aeb3b479d10432bfa
Demian <53890358+demian35@users.noreply.github.com>
2023-07-11 00:17:01 -0600

    insertando el pie de pagina en el index

--- index.php
+++ index.php
@@ -10,5 +10,9 @@
         include("header.php");//importando la clase header para poder ejecutatla aqui
     ?>
     <h1>Bienvenido Dewitt a tu galeria</h1>
+
+    <?php
+        include("fotter.php");//importando la clase footer para insertar el pie de pagina en el index
+    ?>
 </body>
 </html>
