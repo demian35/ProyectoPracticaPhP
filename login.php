3612fa355ac4d7f86e9e7247e3a3a83f25afd345
Demian <53890358+demian35@users.noreply.github.com>
2023-07-11 01:27:44 -0600

    haciendo el login con clases de bootstap

---
+++ login.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    
+      <title>Inicia Sesion</title>
+      <!-- Required meta tags -->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+      <!-- Bootstrap CSS v5.2.1 -->
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    
+    <style>
+        
+        .card{
+            margin-top: 50px;
+        }
+    </style>
+
+</head>
+    
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                </div>
+                
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">
+                            Logueate
+                        </div>
+                        <div class="card-body">
+                            <form action="login.php" method="post">
+                                Usuario:
+                                <input class="form-control" type="text" name="txtusr" id="" >
+                                Password:
+                                <input class="form-control" type="text" name="contrasenia" id="" >
+                                <br>
+                                <button class="btn btn-success" type="submit">Inicia sesion</button>
+
+                            </form>  
+
+
+                        </div>
+                        <div class="card-footer text-muted">
+                            
+                        </div>
+                    </div>
+
+
+                   
+                </div>
+                <div class="col-md-4">
+
+                </div>
+            </div>
+
+        </div>
+        <form action="login.php" method="post">
+
+        </form>  
+
+    </body>
+    
+    </html>
+</head>
+<body>
+    
+</body>
+</html>
