60b432505c3b04a2b2edbb9f81dd54f1be2a1d4b
Demian <53890358+demian35@users.noreply.github.com>
2023-07-11 00:58:54 -0600

    poniendo un jumbotron de bootstrap

--- header.php
+++ header.php
@@ -7,9 +7,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 <body>
-    <a href="login.php">Login</a>
-    <a href="galeria.php">Galeria</a>
-    <a href="cerrar.php">Cerrar Sesion</a>
+    <div class="container">
+        <a href="login.php">Login</a>
+        <a href="galeria.php">Galeria</a>
+        <a href="cerrar.php">Cerrar Sesion</a>
+        <br>
 
-</body>
-</html>
--- index.php
+++ index.php
@@ -2,7 +2,15 @@
     <?php
         include("header.php");//importando la clase header para poder ejecutatla aqui
     ?>
-    <h1>Bienvenido Dewitt a tu galeria</h1>
+            Bienvenido Dewitt a tu galeria
+            
+            <div class="bg-light p-5 rounded-lg m-3">
+            <h1 class="display-4">Bienvenid@</h1>
+            <p class="lead">Esta es tu galeria privada.</p>
+            <hr class="my-4">
+            <p>Aqui podras guardar y ver todos tus momentos y recuerdos felices</p>
+            </div>
+           
 
     <?php
         include("fotter.php");//importando la clase footer para insertar el pie de pagina en el index
