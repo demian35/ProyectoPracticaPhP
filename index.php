523fb1f604f46f0c643405a1c6d5d97bd4aa0112
Demian <53890358+demian35@users.noreply.github.com>
2023-07-11 00:31:47 -0600

    Aplicando algunas clases de bootstrap

--- index.php
+++ index.php
@@ -1,11 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+
     <?php
         include("header.php");//importando la clase header para poder ejecutatla aqui
     ?>
@@ -14,5 +7,3 @@
     <?php
         include("fotter.php");//importando la clase footer para insertar el pie de pagina en el index
     ?>
-</body>
-</html>
