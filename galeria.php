84b9f0bf5d45309c407cab574022419a67514382
Demian <53890358+demian35@users.noreply.github.com>
2023-07-13 02:01:39 -0600

    aplicando estilos de las clases de bootstrap

--- galeria.php
+++ galeria.php
@@ -1,15 +1,22 @@
 <?php include("header.php") ?>
 
-<form action="galeria.php" method="post">
+<div class="card">
+    <div class="card-header">
+    </div>
+    <div class="card-body">
+        <form action="galeria.php" method="post">
+            Nombre: <input class="form-control" type="text" name="nombre" id="">
+            <br>
+            Imagen: <input class="form-control" type="file" name="archivo" id="">
+            <br>
+            <button class="btn btn-success" type="submit">Envia Archivo</button>
+            <br>
+        </form>
+    </div>
+    <div class="card-footer text-muted">
+        Footer
+    </div>
+</div>
 
-    Nombre: <input type="text" name="nombre" id="">
-    <br>
-    Imagen: <input type="file" name="archivo" id="">
-    <br>
-    <input type="submit" value="Envia archivo">
-    <br>
-    
-
-</form>
 
 <?php include("fotter.php") ?>
