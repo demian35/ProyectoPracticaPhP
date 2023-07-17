5cd810eddbb7d3fc097e98efd1512b5da13f7ff5
Demian <53890358+demian35@users.noreply.github.com>
2023-07-17 02:54:33 -0600

    mostrando proyectos en inicio, practica terminada

--- index.php
+++ index.php
@@ -1,17 +1,34 @@
+<?php
+include("header.php"); //importando la clase header para poder ejecutatla aqui
+include("conexionbd.php");
 
-    <?php
-        include("header.php");//importando la clase header para poder ejecutatla aqui
-    ?>
-            Bienvenido Dewitt a tu galeria
-            
-            <div class="bg-light p-5 rounded-lg m-3">
-            <h1 class="display-4">Bienvenid@</h1>
-            <p class="lead">Esta es tu galeria privada.</p>
-            <hr class="my-4">
-            <p>Aqui podras guardar y ver todos tus proyectos</p>
-            </div>
-           
+$conbd2= new conexionbd; //creamos una nueva conexion con la bd
+$proyectos=$conbd2->consulta("SELECT * FROM album.proyectgalery;") //mostramos todos los registros disponibles en bd
 
-    <?php
-        include("fotter.php");//importando la clase footer para insertar el pie de pagina en el index
-    ?>
+?>
+Bienvenido Dewitt a tu galeria
+
+<div class="bg-light p-5 rounded-lg m-3">
+    <h1 class="display-4">Bienvenid@</h1>
+    <p class="lead">Esta es tu galeria privada.</p>
+    <hr class="my-4">
+    <p>Aqui podras guardar y ver todos tus proyectos</p>
+</div>
+
+<div class="card-group">
+<?php foreach($proyectos as $proyecto){ //imprimimos cada fila y columna de la bd ?>
+    <div class="card">
+        <img  src="img/<?php echo $proyecto['imagen'];//le mostramos la imagen como tal al usuario?>" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title"><?php echo $proyecto['nombre'] //columna de nombre?></h5>
+            <p class="card-text"><?php echo $proyecto['descripcion']//columna descripcion?></p>
+        </div>
+    </div>
+<?php }?>
+   
+</div>
+
+
+<?php
+include("fotter.php"); //importando la clase footer para insertar el pie de pagina en el index
+?>
