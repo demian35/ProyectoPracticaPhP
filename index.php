275a8990bb0eec3e9f5cd2be75fc07a93f09248c
Demian <53890358+demian35@users.noreply.github.com>
2023-09-03 00:06:59 -0600

    funcion que imprime el username con la bienvenida

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 include("header.php"); //importando la clase header para poder ejecutatla aqui
 include("conexionbd.php");
 
+
+// Verifica si el usuario ha iniciado sesión
+if (isset($_SESSION['usuario'])) {
+    $usuarioLogueado = $_SESSION['usuario'];
+} else {
+    // Si el usuario no ha iniciado sesión, puedes redirigirlo a la página de inicio de sesión
+    header("location: login.php");
+    exit(); // Asegura que el script se detenga después de la redirección
+}
+
 $conbd2= new conexionbd; //creamos una nueva conexion con la bd
 $proyectos=$conbd2->consulta("SELECT * FROM album.proyectgalery;") //mostramos todos los registros disponibles en bd
 
@@ -9,8 +19,8 @@ $proyectos=$conbd2->consulta("SELECT * FROM album.proyectgalery;") //mostramos t
 Bienvenido a la galeria galeria
 
 <div class="bg-gradient p-5 rounded-lg m-3">
-    <h1 class="display-4" ><strong> Bienvenid@</strong></h1>
-    <p class="lead" ><strong> Esta es tu galeria privada.</strong></p>
+    <h1 class="display-4" ><strong> Bienvenid@, <?php echo $usuarioLogueado; ?> </strong></h1>
+    <p class="lead" ><strong> Galeria de futuros proyectos que se llevaran a cabo en el mundo.</strong></p>
     <hr class="my-4">
     <p><strong> podras guardar y ver todos tus proyectos</strong></p>
 </div>
