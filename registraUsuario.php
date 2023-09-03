9f306b9186c171632622bdc69448b6743b2d2229
Demian <53890358+demian35@users.noreply.github.com>
2023-09-02 22:21:55 -0600

    creando la vista del reigstro de usuario

--- registraUsuario.php
+++ registraUsuario.php
@@ -10,9 +10,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <style>
         body{
-            background-image: url('img/rapture2.jpeg');
-            background-size: 1370px 1200px;
-            background-position: center;
+            background-image: url('img/1689586144barflotante.jpg');
+            background-size: 1370px 700px;
+
         }
     </style>
     <title>Registrate</title>
